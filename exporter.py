84faf73d50a1dd160ad613a6dbe892c9d9f51aef
kerbless <kerbless@proton.me>
2025-12-19 11:39:29 +0100

    not working example of exporter

--- exporter.py
+++ exporter.py
@@ -1,41 +1,36 @@
 #!/usr/bin/python3
 
 # Utility imports
-import subprocess
 import sys
 
 # Import prometheus python library
 from prometheus_client import Gauge, start_http_server
 
-# indirizzi modbus dei multimetri
+# Indirizzi modbus dei multimetri
 rs485_device_ids = {"generale": 3, "gruppo_frigo": 4}
+
+# Multimetri come dizionario nome - gauge
+gauges = {
+    name: Gauge(f"multimetro_{name}", f"Punto misura del multimetro {name}")
+    for name in rs485_device_ids
+}
+
+# Debug print
+print("Exporter multimetri avviato, configurazione:\nMultimetro - id (rs485) - Gauge")
+for name in rs485_device_ids:
+    print(f"{name}, {rs485_device_ids[name]}, {gauges[name]}")
 
 
 # modbus reader function
 def getModbusData(id_device):
-    template_command = f"modbus -s {id_device} -b 38400 -P e -p 1 -r registers.modbus /dev/ttyUSB0 \*"  # TODO: ask for output of THIS command
-    output = subprocess.run(template_command, shell=True, capture_output=True).stdout
-    return output.decode("utf-8").split("\n")[1:]
+    return 1
 
 
 if __name__ == "__main__":
     # Open HTTP server with port 8000
-    server, server_thread = start_http_server(8000)
+    server, server_thread = start_http_server(8400)
 
-    # Create Gauge element for each device id
-    multimeters_gauge = {"generale": object, "gruppo_frigo": object}
-    for el in rs485_device_ids:
-        multimeters_gauge[el] = Gauge(
-            f"multimetro-{el}", f"Punto misura del multimetro {el}"
-        )
-
-    #
-    for line in get:
-        try:
-            lab, val = line.split(":")
-            print(f'multimetri-{lab}{{punto_misura="{el}"}} = {float(val):.2f}')
-        except:
-            pass
+    gauges["generale"].set(4.2)  # Set to a given value
 
     # Graceful exit (just to practice)
     server.shutdown()
