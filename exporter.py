471a8a37d41f5c47ba76a3a1b6aa4b215a5171ff
kerbless <kerbless@proton.me>
2025-12-29 18:24:24 +0100

    Update exporter.py

--- exporter.py
+++ exporter.py
@@ -2,23 +2,43 @@
 
 # Utility imports
 import asyncio
+import
 import random
 import sys
 
-# Import prometheus python library
+# Import modbus and prometheus libraries
+import pymodbus.client as ModbusClient
 from prometheus_client import Gauge, start_http_server
+from pymodbus import (
+    FramerType,
+    ModbusException,
+    pymodbus_apply_logging_config,
+)
 
 # Indirizzi modbus dei multimetri
 rs485_device_ids = {"generale": 3, "gruppo_frigo": 4}
 
-# Metriche esposte {nome, descrizione}
-metrics = {
-    "ingresso1_amps": "Line 1 current (A)",
-    "ingresso2_amps": "Line 2 current (A)",
-    "ingresso3_amps": "Line 3 current (A)",
-    "volts_l1_l2": "Voltage L1-L2 (V)",
-    "power_watts_total": "Total active power (W)",  # would best practice to export joules
-}  # TODO: add all
+# Creazione client pymodbus per ciascun multimetro
+pymodbus_clients = {}
+for multimeter in rs485_device_ids:
+    # https://pymodbus.readthedocs.io/en/latest/source/client.html#pymodbus.client.ModbusSerialClient
+    pymodbus_clients[multimeter] = ModbusClient.ModbusSerialClient(
+        "/dev/ttyUSB0",  # port
+        # timeout=10,
+        # retries=3,
+        baudrate=38400,
+        bytesize=8,
+        parity="E",  # parity: even
+        stopbits=1,
+        # handle_local_echo=False,
+    )
+
+# Metriche da leggere da json
+with open("modbus.json", "r") as file:
+    metrics = json.load(file)
+
+print(metrics)
+
 
 # Labels di cui avremo delle istanze (usare entrambe non duplica i dati)
 labels = ["multimeter", "id"]
@@ -45,6 +65,13 @@ async def getModbusData(metric, id):
     return random.randint(1, 10)
 
 
+# Read https://pymodbus.readthedocs.io/en/latest/source/client.html#client-performance
+# We are using the syncronous function as in our case
+# the bottleneck is on the transmission so we don't care about
+# asyncronous performance right now
+# Read https://pymodbus.readthedocs.io/en/latest/source/client.html#serial-rs-485
+
+
 async def main():
     # Open HTTP server with port 8400
     server, server_thread = start_http_server(8400)
@@ -52,7 +79,7 @@ async def main():
     while True:
         for metric, gauge in metric_gauges.items():
             for name, id in rs485_device_ids.items():
-                updateValue = await getModbusData(metric, id)
+                updateValue = client.read_holding_registers(248, 4, unit=1)
 
                 # gauge.labels(name, id).set_to_current_time() # TODO: confirm that this is done automatically when .set
                 gauge.labels(name, id).set(updateValue)
